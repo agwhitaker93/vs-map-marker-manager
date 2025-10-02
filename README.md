4afea4f99bcc6ab378c50692280f8aab9d8dd8e8
Andrew Whitaker <agwhitaker-dev@protonmail.com>
2025-10-03 00:11:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Map Marker Manager
+
+Makes it easier to manage and use map markers
---
+++ symlink.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env elvish
+
+var mod_dir = $E:HOME/.config/VintagestoryData/Mods
+
+ln -s (pwd)/Releases/mapmarkermanager $mod_dir/vs-map-marker-manager
